655089bf08702a244880c540b734c054fb33ce99
RipMyAcc <phutheneet@gmail.com>
2022-03-16 12:59:28 +0700

    sach

---
+++ src/k14dcpm02/oo/buoi9/book/SachTestDrive.java
@@ -0,0 +1,10 @@
+package k14dcpm02.oo.buoi9.book;
+
+public class SachTestDrive {
+    public static void main(String[] args) {
+        QuanLySach qlsach = new QuanLySach();
+        qlsach.nhapThongTinSGK();
+        qlsach.inThongTinSGK();
+        qlsach.tinhTongThanhTienSGK();
+    }
+}
